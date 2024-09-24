971f31d006b075f44deaa86a2cb4ec829da02f7c
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-24 08:53:00 -0300

    feat: add constant message

---
+++ src/main/java/com/iagomesquita/notificacao/app/constant/MessagesContant.java
@@ -0,0 +1,6 @@
+package com.iagomesquita.notificacao.app.constant;
+
+public class MessagesContant {
+  public static final String PROPOSAL_UNDER_ANALISYS = "Olá, %s espero que estava bem. Sua proposta foi recebida por nossa equipe e esta em fase de Analise. Em breve retornaremos com o resultado.";
+
+}
