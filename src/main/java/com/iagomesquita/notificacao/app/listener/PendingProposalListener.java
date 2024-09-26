64351f189ce154683a2cd04f5061ed386f9a095a
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-26 20:09:10 -0300

    feat: use  Notification Service  in Rabbit litener

--- src/main/java/com/iagomesquita/notificacao/app/listener/PendingProposalListener.java
+++ src/main/java/com/iagomesquita/notificacao/app/listener/PendingProposalListener.java
@@ -2,17 +2,29 @@ package com.iagomesquita.notificacao.app.listener;
 
 import com.iagomesquita.notificacao.app.constant.MessagesContant;
 import com.iagomesquita.notificacao.app.domain.Proposal;
+import com.iagomesquita.notificacao.app.service.NotificationSNSService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PendingProposalListener {
 
+  private NotificationSNSService notificationSNSService;
+
+  public PendingProposalListener(NotificationSNSService notificationSNSService) {
+    this.notificationSNSService = notificationSNSService;
+  }
+
   @RabbitListener(queues = "${rabbitmq.queue.pending-proposal.ms-notification}")
   public void pendingProprosal(Proposal proposal) {
     String message = String.format(
         MessagesContant.PROPOSAL_UNDER_ANALISYS, proposal.getUsuario().getNome());
+    String phoneNumber = proposal.getUsuario().getTelefone();
+
+    notificationSNSService.notifyPendingProposal(phoneNumber, message);
   }
+
+
 
 }
 
